c9f6d9f3f4cda85cdd824f6600582555536cf39f
isaaclyra <isaac.lyra.junior@gmail.com>
2023-03-12 14:15:00 -0300

    feat: Declaracao dos metodos da ED Array List

--- u1-golang/lists/arraylist/arraylist.go
+++ u1-golang/lists/arraylist/arraylist.go
@@ -1,1 +1,42 @@
 package arraylist
+
+import "fmt"
+
+type ArrayList struct {
+	vetor      [10]int
+	quantidade int
+	capacidade int
+}
+
+func (arrayList ArrayList) obterElementoEmPosicao(posicao int) int {
+	//TODO
+	return 0
+}
+
+func (arrayList ArrayList) duplicarCapacidade() {
+	//TODO
+}
+
+func (arrayList ArrayList) inserirElementoNoFim(valor int) {
+	//TODO
+}
+
+func (arrayList ArrayList) inserirElementoEmPosicao(valor int, posicao int) {
+	//TODO
+}
+
+func (arrayList ArrayList) atualizarElemento(valor int, posicao int) {
+	//TODO
+}
+
+func (arrayList ArrayList) removerElementoNoFim() {
+	//TODO
+}
+
+func (arrayList ArrayList) removerElementoEmPosicao(posicao int) {
+	//TODO
+}
+
+func exibirLista(arrayList ArrayList) {
+	fmt.Println(arrayList)
+}
